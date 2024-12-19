5b01ecc1c08b5d73f703ff3a6a2a8e1ed90589d2
Maia Lewis Meza <maia_j_lewismeza@apple.com>
2024-12-19 11:54:27 -0800

    google sign in working

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+'use client'
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
+});
